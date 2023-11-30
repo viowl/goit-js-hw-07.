3942b04a846abc7ded9a4760dce358bc51eda32c
viowl <sovaworkbox@gmail.com>
2023-11-30 20:36:24 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categories = document.getElementById('categories');
+const item = document.querySelector('.item')
+const arry = [...categories.children];
+console.log(arry);
+console.log(item);
+console.dir(categories);
+
+console.log(categories.children.length)
