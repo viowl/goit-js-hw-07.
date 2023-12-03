fa043d6005c94cfbf1d43710c48aaab30e0b7327
viowl <sovaworkbox@gmail.com>
2023-12-03 02:08:19 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+
+  loginForm.addEventListener('submit', onLogin)
+
+  function onLogin(event) {
+    event.preventDefault(); // Зупиняємо перезавантаження сторінки
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+      alert('Будь ласка, заповніть всі поля');
+    } else {
+      const loginData = {
+        email: emailInput.value,
+        password: passwordInput.value,
+      };
+      console.log(loginData);
+      loginForm.reset(); // Очищаємо значення полів форми
+    }
+  };
 
