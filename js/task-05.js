0bdc820293a89b7549a78b9f5087d3b22d55d013
viowl <sovaworkbox@gmail.com>
2023-12-03 02:11:47 +0100

    finish hw

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+
+button.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+};
--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+  
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
